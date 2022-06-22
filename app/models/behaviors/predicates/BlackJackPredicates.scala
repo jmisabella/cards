eef555190e5b01859e6fd59df228ea5c561795f3
Jeffrey <jeff.isabella@gmail.com>
2022-06-21 20:36:53 -0400

    added initial blackjack predicates behavior
    added initial blackjack predicates behavior

---
+++ app/models/behaviors/predicates/BlackJackPredicates.scala
@@ -0,0 +1,23 @@
+package cards.models.behaviors.predicates
+
+import cards.models.behaviors.Commons
+import cards.models.classes.{ Card, Rank, Suit }
+import cards.models.classes.Rank._
+import cards.models.classes.Suit._
+
+trait BlackJackPredicates {
+  type CB <: Commons
+  val commons: CB 
+
+  // only players can split on first play (only 2 cards) when both cards match rank
+  // dealers cannot split, but this function doesn't check for this
+  def canSplit(playerCards: Seq[Card]): Boolean = 
+    playerCards.length == 2 && commons.countRank(playerCards).values.toSeq.contains(2)
+
+  // players eligible for insurance only if dealer's first hand (only 2 cards) shows an ace as the face-up card  
+  def eligibleForInstance(dealerFaceUpCards: Seq[Card]): Boolean =
+    // only eligible if first turn (dealer only has 1 face up card) and the face up card is an Ace 
+    dealerFaceUpCards.length == 1 && dealerFaceUpCards.head.rank == Ace
+
+
+}
--- app/models/behaviors/predicates/PokerPredicates.scala
+++ app/models/behaviors/predicates/PokerPredicates.scala
@@ -11,24 +11,33 @@ trait PokerPredicates {
 
   def straight(cards: Seq[Card]): Boolean = commons.sequenced(cards)
 
-  def flush(cards: Seq[Card]): Boolean = commons.countSuit(cards).values.toSeq.contains(cards.length)
+  def flush(cards: Seq[Card]): Boolean = 
+    commons.countSuit(cards).values.toSeq contains cards.length
 
-  def straightFlush(cards: Seq[Card]): Boolean = straight(cards) && flush(cards)
+  def straightFlush(cards: Seq[Card]): Boolean = 
+    straight(cards) && flush(cards)
 
-  def royalFlush(cards: Seq[Card]): Boolean = straightFlush(cards) && 
+  def royalFlush(cards: Seq[Card]): Boolean = 
+    straightFlush(cards) && 
     cards.count(c => Seq(Ten, Jack, Queen, King, Ace).contains(c.rank)) == cards.length
 
-  def onePair(cards: Seq[Card]): Boolean = commons.countRank(cards).values.toSeq.count(_ == 2) == 1
+  def onePair(cards: Seq[Card]): Boolean = 
+    commons.countRank(cards).values.toSeq.count(_ == 2) == 1
   
-  def twoPair(cards: Seq[Card]): Boolean = commons.countRank(cards).values.toSeq.count(_ == 2) == 2
+  def twoPair(cards: Seq[Card]): Boolean = 
+    commons.countRank(cards).values.toSeq.count(_ == 2) == 2
 
-  def threeOfAKind(cards: Seq[Card]): Boolean = commons.countRank(cards).values.toSeq.contains(3)
+  def threeOfAKind(cards: Seq[Card]): Boolean = 
+    commons.countRank(cards).values.toSeq contains 3
   
-  def fourOfAKind(cards: Seq[Card]): Boolean = commons.countRank(cards).values.toSeq.contains(4)
+  def fourOfAKind(cards: Seq[Card]): Boolean = 
+    commons.countRank(cards).values.toSeq contains 4
 
-  def fullHouse(cards: Seq[Card]): Boolean = onePair(cards) && threeOfAKind(cards)
+  def fullHouse(cards: Seq[Card]): Boolean = 
+    onePair(cards) && threeOfAKind(cards)
 
-  def highCard(cards: Seq[Card]): Boolean = !onePair(cards) && 
+  def highCard(cards: Seq[Card]): Boolean = 
+    !onePair(cards) && 
     !twoPair(cards) && 
     !straight(cards) && 
     !flush(cards) && 
