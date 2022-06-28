ec5423740225cf8575d1f1010192c615d48b9a5b
Jeffrey <jeff.isabella@gmail.com>
2022-06-27 22:16:59 -0400

    refactor
    refactor

--- app/models/classes/Card.scala
+++ app/models/classes/Card.scala
@@ -18,12 +18,7 @@ import cards.models.classes.Suit._
 
 sealed trait Card {
   val rank: Rank
-  val isJoker: Boolean
-
-  def copy(isJoker: Boolean = this.isJoker): Card = this match {
-    case UnsuitedCard(joker) => UnsuitedCard(joker) 
-    case SuitedCard(rank, suit) => SuitedCard(rank, suit)
-  }
+  val isJoker: Boolean = rank == LeftBower || rank == RightBower 
 }
 
 object Card {
@@ -31,21 +26,17 @@ object Card {
 }
 
 case class SuitedCard(override val rank: Rank, suit: Suit) extends Card {
-  override val isJoker = false
   override def toString(): String = s"$rank of $suit"
   require(!isJoker, s"SuitedCard is not supposed to have isJoker but it does; rank [$rank], suit [$suit]")
-  require(!Seq(LeftBower, RightBower).contains(rank), s"SuitedCard's rank [$rank] is not supposed to be LeftBower or RightBower jokers")
   implicit def ordering[A <: SuitedCard]: Ordering[A] = Ordering.by(_.rank.id)
 }
 
 case class UnsuitedCard(override val rank: Rank) extends Card {
-  override val isJoker = true
   override def toString(): String = rank match {
     case LeftBower => "Left Bower Joker"
     case RightBower => "Right Bower Joker"
   }
   implicit def ordering[A <: UnsuitedCard]: Ordering[A] = Ordering.by(_.rank.id)
   require(isJoker, s"UnsuitedCard is supposed to have isJoker but it does not; rank [$rank]")
-  require(!Seq(Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace).contains(rank), s"UnsuitedCard's rank [$rank] is supposed to only be either LeftBower or RightBower joker, but it's not")
 }
 
