093df8c1e2ace3fad181ee250c107996bbf03c14
Jeffrey <jeff.isabella@gmail.com>
2022-08-05 22:32:35 -0400

    intermittent commit
    intermittent commit

--- test/models/classes/state/ThirtyOneStateSpec.scala
+++ test/models/classes/state/ThirtyOneStateSpec.scala
@@ -5,6 +5,7 @@ import cards.models.classes.{ Card, Rank, Suit, Deck }
 import cards.models.classes.Rank._
 import cards.models.classes.Suit._
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
 
 class ThirtyOneStateSpec extends AnyFlatSpec {
   "ThirtyOneGameState" should "determine current player and next player" in {
@@ -14,17 +15,17 @@ class ThirtyOneStateSpec extends AnyFlatSpec {
       Seq(Card(Eight, Spades), Card(Nine, Spades), Card(Ten, Spades)))
     val players = Seq(ThirtyOnePlayerState("player1", 3, hands(0)), ThirtyOnePlayerState("player2", 3, hands(1)), ThirtyOnePlayerState("player3", 3, hands(2))) 
     val initialState = ThirtyOneGameState(players = players, currentPlayerIndex = Some(0))
-    assert(initialState.currentPlayer() == players(0))
-    assert(initialState.players(initialState.nextPlayerIndex()) == players(1))
+    initialState.currentPlayer() should equal (players(0))
+    initialState.players(initialState.nextPlayerIndex()) should equal (players(1))
     val nextState = initialState.copy(currentPlayerIndex = Some(initialState.nextPlayerIndex()))
-    assert(nextState.currentPlayer() == players(1))
-    assert(nextState.players(nextState.nextPlayerIndex()) == players(2))
+    nextState.currentPlayer() should equal (players(1))
+    nextState.players(nextState.nextPlayerIndex()) should equal (players(2))
     val nextNextState = nextState.copy(currentPlayerIndex = Some(nextState.nextPlayerIndex()))
-    assert(nextNextState.currentPlayer() == players(2))
-    assert(nextNextState.players(nextNextState.nextPlayerIndex()) == players(0))
+    nextNextState.currentPlayer() should equal (players(2))
+    nextNextState.players(nextNextState.nextPlayerIndex()) should equal (players(0))
     val lastState = nextNextState.copy(currentPlayerIndex = Some(nextNextState.nextPlayerIndex()))
-    assert(lastState.currentPlayer() == players(0))
-    assert(lastState.players(lastState.nextPlayerIndex()) == players(1))
+    lastState.currentPlayer() should equal (players(0))
+    lastState.players(lastState.nextPlayerIndex()) should equal (players(1))
   }
 
   it should "throw an illegal state exception getting current player when current player index isn't specified" in {
@@ -33,13 +34,8 @@ class ThirtyOneStateSpec extends AnyFlatSpec {
       Seq(Card(Five, Hearts), Card(Six, Hearts), Card(Seven, Hearts)), 
       Seq(Card(Eight, Spades), Card(Nine, Spades), Card(Ten, Spades)))
     val players = Seq(ThirtyOnePlayerState("player1", 3, hands(0)), ThirtyOnePlayerState("player2", 3, hands(1)), ThirtyOnePlayerState("player3", 3, hands(2))) 
-    try {
-      val state = ThirtyOneGameState(players = players)
-      state.currentPlayer()
-      assert(false)
-    } catch {
-      case _: IllegalStateException => assert(true)
-    }
+    val state = ThirtyOneGameState(players = players) 
+    an [IllegalStateException] should be thrownBy state.currentPlayer()
   }
   
   it should "throw an illegal state exception getting next player index when current player index isn't specified" in {
@@ -48,35 +44,20 @@ class ThirtyOneStateSpec extends AnyFlatSpec {
       Seq(Card(Five, Hearts), Card(Six, Hearts), Card(Seven, Hearts)), 
       Seq(Card(Eight, Spades), Card(Nine, Spades), Card(Ten, Spades)))
     val players = Seq(ThirtyOnePlayerState("player1", 3, hands(0)), ThirtyOnePlayerState("player2", 3, hands(1)), ThirtyOnePlayerState("player3", 3, hands(2))) 
-    try {
-      val state = ThirtyOneGameState(players = players)
-      state.nextPlayerIndex()
-      assert(false)
-    } catch {
-      case _: IllegalStateException => assert(true)
-    }
+    val state = ThirtyOneGameState(players = players)
+    an [IllegalStateException] should be thrownBy state.nextPlayerIndex() 
   }
 
   it should "throw an illegal state exception getting current player when players list is empty" in {
     val players: Seq[ThirtyOnePlayerState] = Nil
-    try {
-      val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(0))
-      state.currentPlayer()
-      assert(false)
-    } catch {
-      case _: IllegalStateException => assert(true)
-    }
+    val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(0))
+    an [IllegalStateException] should be thrownBy state.currentPlayer()
   }
 
   it should "throw an illegal state exception getting next player index when players list is empty" in {
     val players: Seq[ThirtyOnePlayerState] = Nil
-    try {
-      val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(0))
-      state.nextPlayerIndex()
-      assert(false)
-    } catch {
-      case _: IllegalStateException => assert(true)
-    }
+    val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(0))
+    an [IllegalStateException] should be thrownBy state.nextPlayerIndex() 
   }
 
   it should "throw an illegal state exception getting current player when currentPlayerIndex is out of range" in {
@@ -85,13 +66,8 @@ class ThirtyOneStateSpec extends AnyFlatSpec {
       Seq(Card(Five, Hearts), Card(Six, Hearts), Card(Seven, Hearts)), 
       Seq(Card(Eight, Spades), Card(Nine, Spades), Card(Ten, Spades)))
     val players = Seq(ThirtyOnePlayerState("player1", 3, hands(0)), ThirtyOnePlayerState("player2", 3, hands(1)), ThirtyOnePlayerState("player3", 3, hands(2))) 
-    try {
-      val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(3))
-      state.currentPlayer()
-      assert(false)
-    } catch {
-      case _: IllegalStateException => assert(true)
-    }
+    val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(3))
+    an [IllegalStateException] should be thrownBy state.currentPlayer()
   }
 
   it should "NOT throw an exception getting next player index when currentPlayerIndex is out of range, and instead use mod to reset back to the beginning" in {
@@ -100,13 +76,10 @@ class ThirtyOneStateSpec extends AnyFlatSpec {
       Seq(Card(Five, Hearts), Card(Six, Hearts), Card(Seven, Hearts)), 
       Seq(Card(Eight, Spades), Card(Nine, Spades), Card(Ten, Spades)))
     val players = Seq(ThirtyOnePlayerState("player1", 3, hands(0)), ThirtyOnePlayerState("player2", 3, hands(1)), ThirtyOnePlayerState("player3", 3, hands(2))) 
-    try {
-      val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(3))
-      val nextIndex: Int = state.nextPlayerIndex()
-      assert(nextIndex == 1)
-    } catch {
-      case _: IllegalStateException => assert(false)
-    }
+    val state = ThirtyOneGameState(players = players, currentPlayerIndex = Some(3))
+    info("an illegal state exception should not be thrown by next player index") 
+    val nextIndex: Int = state.nextPlayerIndex()
+    nextIndex should equal (1)
   }
 
   it should "determine current and next player to be the same player when there is only a single player" in {
