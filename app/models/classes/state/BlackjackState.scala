eb731b0854ba5d953501e9e1aad8035ae3230514
Jeffrey <jeff.isabella@gmail.com>
2022-08-16 00:32:13 -0400

    intermittent commit
    intermittent commit

--- app/models/classes/state/BlackjackState.scala
+++ app/models/classes/state/BlackjackState.scala
@@ -9,8 +9,21 @@ import cards.models.classes.options.ResplitLimit._
 import cards.models.classes.actions.{ Action, BlackjackAction }
 import cards.models.classes.actions.BlackjackAction._
 
-// bets' key is tuple with player's id and hand index as the key, and the amount bet as the value
-case class BlackjackPlayerState(id: String, bank: Int, hands: Seq[Seq[Card]] = Nil, bets: Map[(String, Int), Int] = Map())
+// handsAndBets: sequence of tuples, where each tuple's _1 is the actual hand, and the tuple's _2 is bets placed on the hand (map with player id as key and bet placed as value)
+case class BlackjackPlayerState(id: String, bank: Int = 0, handsAndBets: Seq[(Seq[Card], Map[String, Int])] = Nil) {
+  val hands: Seq[Seq[Card]] = handsAndBets.map(_._1)
+  // TODO: test 
+  def playerBets(playerId: String): Seq[(Seq[Card], Int)] = { 
+    for (x <- handsAndBets if (x._2.keys.toSeq.contains(playerId))) yield {
+      (x._1, x._2.filter(_._1 == playerId).values.head)
+    }
+  }
+}
+
+object BlackjackPlayerState {
+  def apply(hands: Seq[Seq[Card]], id: String, bank: Int): BlackjackPlayerState = BlackjackPlayerState(id, bank, hands.map(h => (h, Map(): Map[String, Int]))) 
+  def apply(id: String, hand: Seq[Card], bank: Int): BlackjackPlayerState = BlackjackPlayerState(Seq(hand), id, bank) 
+}
 
 // dealer's hand's head is the face-up card, all other cards are face down
 case class BlackjackGameState(
