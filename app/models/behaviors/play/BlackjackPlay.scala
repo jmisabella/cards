5ece632a24b359d50e5622ddd7d0b62f726e54b3
Jeffrey <jeff.isabella@gmail.com>
2022-09-09 21:15:53 -0400

    BlackjackPlay behavior: isTimeToPlay()
    BlackjackPlay behavior: isTimeToPlay()

--- app/models/behaviors/play/BlackjackPlay.scala
+++ app/models/behaviors/play/BlackjackPlay.scala
@@ -1,7 +1,35 @@
 package cards.models.behaviors.play
 
-trait BlackjackPlay {
+import cards.models.classes.state.{ BlackjackGameState, BlackjackPlayerState }
+import cards.models.classes.{ Card, Rank, Suit, Deck }
 
-  // TODO: 
+trait BlackjackPlay {
+  
+  def getPlayerBet(playerState: BlackjackPlayerState, playerId: String): Option[(Seq[Card], Int)] = { 
+    (for {
+      x <- playerState.handsAndBets
+      if (x.bets.keys.toSeq.contains(playerId))
+    } yield (x.hand, x.bets.filter(_._1 == playerId).values.head)
+    ).headOption
+  }
+
+  def getPlayerBets(game: BlackjackGameState, playerId: String): Seq[(Seq[Card], Int)] = {
+    for {
+      player <- game.players
+      bet <- getPlayerBet(player, playerId)
+    } yield bet
+  }
+
+  def hasPlayerBet(game: BlackjackGameState, playerId: String): Boolean = getPlayerBets(game, playerId) != Nil
+
+  def isTimeToPlay(game: BlackjackGameState): Boolean = {
+    if (game.players == Nil)
+      throw new IllegalArgumentException("cannot determine whether isTimeToPlay when there are no players")
+    (for {
+      player <- game.players
+    } yield hasPlayerBet(game, player.id))
+      .count(p => p) == game.players.length && 
+      (game.players.flatMap(_.handsAndBets.map(h => h.wins)).count(w => w.isDefined) != game.players.length)
+  }
 
 }
--- test/models/behaviors/play/BlackjackPlaySpec.scala
+++ test/models/behaviors/play/BlackjackPlaySpec.scala
@@ -6,6 +6,7 @@ import cards.models.classes.DeckType._
 import cards.models.classes.Rank._
 import cards.models.classes.Suit._
 import cards.models.classes.hand.Hand
+import cards.models.classes.options.BlackjackOptions
 import cards.models.classes.state.{ BlackjackGameState, BlackjackPlayerState }
 import cards.models.classes.options.BlackjackPayout._
 import cards.models.classes.actions.{ Action, BlackjackAction }
@@ -19,19 +20,89 @@ class BlackjackPlaySpec extends AnyFlatSpec with GivenWhenThen {
   import _play._
 
   "BlackjackPlay" should "know when it's not yet time to play because bets have not yet been taken" in {
-
-    pending
+    Given("a game with 2 players but who have not yet placed any bets")
+    val player1 = BlackjackPlayerState(
+      "Jeffrey", 
+      25, 
+      Seq( 
+        Hand()))
+    val player2 = BlackjackPlayerState(
+      "Alice", 
+      50, 
+      Seq( 
+        Hand()))
+    val game = BlackjackGameState(options = BlackjackOptions(), dealerHand = Hand(), players = Seq(player1, player2), currentPlayerIndex = Some(0))
+    When("determining whether it's time to play game")
+    val timeToPlay: Boolean = isTimeToPlay(game)
+    Then("it's determined that it's not yet time to play because no bets have been taken by either player")
+    timeToPlay shouldBe (false) 
   }
 
+  it should "know when it's not yet time to play because only 1 out of 2 players have placed their bets" in {
+    Given("a game with 2 players and only the first player has placed bets while 2nd player has not yet placed any bets")
+    val player1 = BlackjackPlayerState(
+      "Jeffrey", 
+      25, 
+      Seq( 
+        Hand(hand = Nil, bets = Map("Jeffrey" -> 10))))
+    val player2 = BlackjackPlayerState(
+      "Alice", 
+      50, 
+      Seq( 
+        Hand()))
+    val game = BlackjackGameState(options = BlackjackOptions(), dealerHand = Hand(), players = Seq(player1, player2), currentPlayerIndex = Some(0))
+    When("determining whether it's time to play game")
+    val timeToPlay: Boolean = isTimeToPlay(game)
+    Then("it's determined that it's not yet time to play because not all bets have been taken")
+    timeToPlay shouldBe (false) 
+  }
+  
+  it should 
+  "know when it's time to play because player's bet has been taken but player has not yet been dealt any cards" in {
+    Given("a game with 1 player who has placed minimum bet but has not yet been dealt any cards")
+    val player1 = BlackjackPlayerState(
+      "Jeffrey", 
+      25, 
+      Seq( 
+        Hand(hand = Nil, bets = Map("Jeffrey" -> 5))))
+    val player2 = BlackjackPlayerState(
+      "Alice", 
+      50, 
+      Seq( 
+        Hand(hand = Nil, bets = Map("Alice" -> 5))))
+    val game = BlackjackGameState(options = BlackjackOptions(), minimumBet = 5, dealerHand = Hand(), players = Seq(player1, player2), currentPlayerIndex = Some(0))
+    When("determining whether it's time to play game")
+    val timeToPlay: Boolean = isTimeToPlay(game)
+    Then("it's determined that it's indeed time to play")
+    timeToPlay shouldBe (true) 
+  }
+  
   it should 
   "know when it's time to play because bets have been taken and the player has 2 or more cards and hand is not yet flagged as either 'won' or 'lost'" in {
-
-    pending
+    Given("a game with 1 player who has 2 cards and his hand his not flagged as 'won' or 'lost'")
+    val player1 = BlackjackPlayerState(
+      "Jeffrey", 
+      25, 
+      Seq( 
+        Hand(hand = Seq(Card(Two, Hearts), Card(Ten, Diamonds)), bets = Map("Jeffrey" -> 5), wins = None)))
+    val player2 = BlackjackPlayerState(
+      "Alice", 
+      50, 
+      Seq( 
+        Hand(hand = Seq(Card(Nine, Clubs), Card(Jack, Clubs)), bets = Map("Alice" -> 5), wins = None)))
+    val game = BlackjackGameState(options = BlackjackOptions(), minimumBet = 5, dealerHand = Hand(), players = Seq(player1, player2), currentPlayerIndex = Some(0))
+    When("determining whether it's time to play game")
+    val timeToPlay: Boolean = isTimeToPlay(game)
+    Then("it's determined that it's indeed time to play")
+    timeToPlay shouldBe (true) 
   }
 
   it should "throw an illegal argument exception when attempting to play but there are no players" in {
-
-    pending
+    Given("a game with no players")
+    val game = BlackjackGameState(options = BlackjackOptions(), minimumBet = 5, dealerHand = Hand(), players = Nil)
+    When("determining whether it's time to play game")
+    Then("an illegal state exception should be thrown")
+    an [IllegalArgumentException] shouldBe thrownBy (isTimeToPlay(game))
   }
 
   // TODO: 
